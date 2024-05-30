0fe8a55ee7e291c119593a2c71be12ff1cad538f
prithviseran <prithisha33@gmail.com>
2024-05-30 17:42:07 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,26 +1,58 @@
+import java.io.IOException;
+
 public class Driver {
-    public static void main(String [] args) {
 
-        Polynomial p = new Polynomial();
+	public static void main(String [] args) {
 
-        System.out.println(p.evaluate(3));
 
-        double [] c1 = {6,0,0,5};
+		Polynomial test6 = new Polynomial(new double[] {1.12, -2, 3}, new int[] {4, 3, 2});
 
-        Polynomial p1 = new Polynomial(c1);
+        Polynomial test7 = new Polynomial(new double[] {-4.5, 99}, new int[] {1, 99});
 
-        double [] c2 = {0,-2,0,0,-9};
+        
+        Polynomial result3 = test7.multiply(test6);
 
-        Polynomial p2 = new Polynomial(c2);
+        try{
+            result3.saveToFile("c.txt");
 
-        Polynomial s = p1.add(p2);
-
-        System.out.println("s(0.1) = " + s.evaluate(0.1));
-        if(s.hasRoot(1)){
-            System.out.println("1 is a root of s");
         }
-        else{
-            System.out.println("1 is not a root of s");
+        catch (IOException ex){
+            System.out.println("Error Occured");
         }
-    }
+
+        System.out.println(result3.evaluate(-23.5));
+
+        result3 = test6.multiply(test7);
+
+        System.out.println(result3.evaluate(-23.5));
+
+        result3 = test6.add(test7);
+
+        System.out.println(result3.evaluate(23.5));
+
+        result3 = test7.add(test6);
+
+        System.out.println(result3.evaluate(23.5));
+
+
+        try {
+            result3.saveToFile("b.txt");
+
+        }
+        catch (IOException ex){
+            System.out.println("Error Occured");
+        }
+        
+
+
+        //Output:
+
+        //-1.9888575968676583E143
+        //-1.9888575968676583E143
+        //5.387081668148581E137
+        //5.387081668148581E137
+        // open c.txt: "110.88000000000001x103-198.0x102+297.0x101-5.040000000000001x5+9.0x4-13.5x3"
+        //open b.txt: "1.12x4-2.0x3+3.0x2-4.5x1+99.0x99"
+		
+	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,66 +1,253 @@
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.IOException;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Polynomial{
-    double[] coeffecients;
+    double[] coefficients;
+    int[] exponents;
+
 
     Polynomial(){
-        this.coeffecients = new double[1];
-        this.coeffecients[0] = 0;
+        this.coefficients = new double[1];
+        this.coefficients[0] = 0;
+
+        this.exponents = new int[1];
+        this.exponents[0] = 0;
     }
 
-    Polynomial(double[]coeffecients_in){
-        this.coeffecients = new double[coeffecients_in.length];
+    Polynomial(double[]coeffecients_in, int[]exponents_in){
+
+        this.coefficients = new double[coeffecients_in.length];
+        this.exponents = new int[exponents_in.length];
+
 
         for (int i = 0; i < coeffecients_in.length; i++){
-            this.coeffecients[i] = coeffecients_in[i];
+            this.coefficients[i] = coeffecients_in[i];
         }
+
+        for (int i = 0; i < exponents_in.length; i++){
+            this.exponents[i] = exponents_in[i];
+        }
+        
+        //coefficients_in;
+        //this.exponents = exponents_in;
 
     }
 
-    public Polynomial add(Polynomial pol1){
+    
+    public Polynomial(File file) throws IOException{
+        BufferedReader scanner = new BufferedReader(new FileReader(file));
+        String line = scanner.readLine();
+        scanner.close();
 
-        int lowest_length = Math.min(pol1.coeffecients.length, coeffecients.length);
-        int highest_length = Math.max(pol1.coeffecients.length, coeffecients.length);
-        double[] new_array = new double[highest_length];
+        ArrayList<Double> coeffsList = new ArrayList<>();
+        ArrayList<Integer> expsList = new ArrayList<>();
 
-        for (int i = 0; i < lowest_length; i++){
-            new_array[i] = this.coeffecients[i] + pol1.coeffecients[i];
-        }
+        String[] terms = line.split("(?=[+-])"); // Split by + or - while keeping them
 
-        if (highest_length == pol1.coeffecients.length){
-            for (int i = lowest_length; i < highest_length; i++){
-                new_array[i] = pol1.coeffecients[i];
+        for (String term : terms) {
+            term = term.trim();
+            if (term.equals("")) continue;
+
+            if (term.contains("x")) {
+                String[] parts = term.split("x");
+
+                double coeff;
+                if (parts[0].equals("+") || parts[0].equals("") || parts[0].equals("-")) {
+                    coeff = parts[0].equals("-") ? -1 : 1;
+                } else {
+                    coeff = Double.parseDouble(parts[0]);
+                }
+
+                int exp;
+                if (parts.length == 1 || parts[1].equals("")) {
+                    exp = 1;
+                } else {
+                    exp = Integer.parseInt(parts[1].replace("^", ""));
+                }
+
+                coeffsList.add(coeff);
+                expsList.add(exp);
+            } else {
+                coeffsList.add(Double.parseDouble(term));
+                expsList.add(0);
             }
         }
-        else{
-            for (int i = lowest_length; i < highest_length; i++){
-                new_array[i] = this.coeffecients[i];
-            }
+
+        this.coefficients = new double[coeffsList.size()];
+        this.exponents = new int[expsList.size()];
+
+        for (int i = 0; i < coeffsList.size(); i++) {
+            this.coefficients[i] = coeffsList.get(i);
+            this.exponents[i] = expsList.get(i);
         }
-
-
-        Polynomial result = new Polynomial(new_array);
-
-        return result;
-
     }
 
-    public double evaluate(double x){
+    public void saveToFile(String fileName) throws IOException {
+        PrintWriter writer = new PrintWriter(new File(fileName));
+        String sb = "";
+        String total = "";
+
+        for (int i = 0; i < this.coefficients.length; i++) {
+            double coeff = this.coefficients[i];
+            int exp = this.exponents[i];
+
+            if (coeff > 0 && i != 0) {
+                sb = sb + "+";
+            }
+            if (exp == 0) {
+                sb = sb + coeff;
+            } 
+            else if (exp == 1) {
+                if (coeff == 1) {
+                    sb = sb + "x";
+                } else if (coeff == -1) {
+                    sb = sb + "-x";
+                } else {
+                    sb = sb + coeff + "x";
+                }
+            }   
+            else {
+                if (coeff == 1) {
+                    sb = sb + "x" + exp;
+                } else if (coeff == -1) {
+                    sb = sb + "-x" + exp;
+                } else {
+                    sb = sb + coeff + "x" + exp;
+                }
+            }
+
+        }
+
+
+        //System.out.println(total);
+
+
+
+        writer.println(sb);
+        writer.close();
+    
+    }
+    
+
+    public Polynomial add(Polynomial pol1) {
+        int newLength = this.coefficients.length + pol1.coefficients.length;
+        double[] tempCoefficients = new double[newLength];
+        int[] tempExponents = new int[newLength];
+        int index = 0;
+
+        // Copy this polynomial's terms
+        for (int i = 0; i < this.coefficients.length; i++) {
+            tempCoefficients[index] = this.coefficients[i];
+            tempExponents[index] = this.exponents[i];
+            index++;
+        }
+
+        // Add the terms from the other polynomial
+        for (int i = 0; i < pol1.coefficients.length; i++) {
+            boolean found = false;
+            for (int j = 0; j < this.coefficients.length; j++) {
+                if (pol1.exponents[i] == this.exponents[j]) {
+                    tempCoefficients[j] += pol1.coefficients[i];
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                tempCoefficients[index] = pol1.coefficients[i];
+                tempExponents[index] = pol1.exponents[i];
+                index++;
+            }
+        }
+
+        // Remove zero coefficients and create the resulting polynomial
+        int nonZeroCount = 0;
+        for (int i = 0; i < index; i++) {
+            if (tempCoefficients[i] != 0) {
+                nonZeroCount++;
+            }
+        }
+
+        double[] finalCoefficients = new double[nonZeroCount];
+        int[] finalExponents = new int[nonZeroCount];
+        int finalIndex = 0;
+
+        for (int i = 0; i < index; i++) {
+            if (tempCoefficients[i] != 0) {
+                finalCoefficients[finalIndex] = tempCoefficients[i];
+                finalExponents[finalIndex] = tempExponents[i];
+                finalIndex++;
+            }
+        }
+
+        return new Polynomial(finalCoefficients, finalExponents);
+    }
+
+    public double evaluate(double x) {
         double sum = 0.0;
-        for (int i = 0; i < coeffecients.length; i++){
-            sum = sum + this.coeffecients[i]*(Math.pow(x, i));
+        for (int i = 0; i < coefficients.length; i++) {
+            sum += coefficients[i] * Math.pow(x, exponents[i]);
         }
-
         return sum;
     }
 
-    public boolean hasRoot(double val){
-        double result = this.evaluate(val);
+    public boolean hasRoot(double val) {
+        return evaluate(val) == 0.0;
+    }
 
-        if (result == 0.0){
-            return true;
+    public Polynomial multiply(Polynomial pol1) {
+        int newLength = this.coefficients.length * pol1.coefficients.length;
+        double[] tempCoefficients = new double[newLength];
+        int[] tempExponents = new int[newLength];
+        int index = 0;
+
+        for (int i = 0; i < this.coefficients.length; i++) {
+            for (int j = 0; j < pol1.coefficients.length; j++) {
+                double newCoeff = this.coefficients[i] * pol1.coefficients[j];
+                int newExp = this.exponents[i] + pol1.exponents[j];
+                
+                boolean found = false;
+                for (int k = 0; k < index; k++) {
+                    if (tempExponents[k] == newExp) {
+                        tempCoefficients[k] += newCoeff;
+                        found = true;
+                        break;
+                    }
+                }
+                
+                if (!found) {
+                    tempCoefficients[index] = newCoeff;
+                    tempExponents[index] = newExp;
+                    index++;
+                }
+            }
         }
 
-        return false;
+        int nonZeroCount = 0;
+        for (int i = 0; i < index; i++) {
+            if (tempCoefficients[i] != 0) {
+                nonZeroCount++;
+            }
+        }
+
+        double[] finalCoefficients = new double[nonZeroCount];
+        int[] finalExponents = new int[nonZeroCount];
+        int finalIndex = 0;
+
+        for (int i = 0; i < index; i++) {
+            if (tempCoefficients[i] != 0) {
+                finalCoefficients[finalIndex] = tempCoefficients[i];
+                finalExponents[finalIndex] = tempExponents[i];
+                finalIndex++;
+            }
+        }
+
+        return new Polynomial(finalCoefficients, finalExponents);
     }
 }
